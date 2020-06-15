73a6a7ece13892ad4238187e46b4dd744ca2137b
theepag <theepag@live.com>
2020-06-15 17:33:36 +0530

    Added Chart But it's not working :P

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,40 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-  return <h1> Chart</h1>;
+  const [dailyData, setDailyData] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+
+    fetchAPI();
+  });
+  const lineChart =
+    dailyData.length != 0 ? (
+      <Line
+        data={{
+          lables: dailyData.map(({ date }) => date),
+          datasets: [
+            {
+              data: dailyData.map(({ confirmed }) => confirmed),
+              lable: "Infected",
+              borderColor: "#3333ff",
+              fill: true,
+            },
+            {
+              data: dailyData.map(({ deaths }) => deaths),
+              lable: "Deaths",
+              borderColor: "red",
+              backgroundColor: "rgba(255,0,0,0.5)",
+              fill: true,
+            },
+          ],
+        }}
+      />
+    ) : null;
+  return <div className={styles.container}>{lineChart}</div>;
 };
 export default Chart;
