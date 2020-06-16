e52c9cc90e083dd79491b3c34459ab0153ae12f5
theepag <theepag@live.com>
2020-06-16 22:12:04 +0530

    Updates

---
+++ precache-manifest.bce301991a63d0c38077bbeb4029e870.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd303e3ced9c5c75465fa34bbfc76d8",
+    "url": "/CovidTracker/index.html"
+  },
+  {
+    "revision": "288897db7f312c8c33e8",
+    "url": "/CovidTracker/static/css/main.c6623552.chunk.css"
+  },
+  {
+    "revision": "b5157362e938b2280a1a",
+    "url": "/CovidTracker/static/js/2.d4398849.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CovidTracker/static/js/2.d4398849.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288897db7f312c8c33e8",
+    "url": "/CovidTracker/static/js/main.c0c9d181.chunk.js"
+  },
+  {
+    "revision": "ac81b8fd1f91f81fb16c",
+    "url": "/CovidTracker/static/js/runtime-main.5cd53096.js"
+  },
+  {
+    "revision": "1ed8bac1d659a339b5d6441e35911f3e",
+    "url": "/CovidTracker/static/media/image.1ed8bac1.png"
+  }
+]);
